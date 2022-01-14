4b9138f5469987c48e10c4b36b73564b874a8d02
Fritz <fritz@strauch.at>
2022-01-14 11:53:46 +0100

    numberofResidents_should_be1_adding_resident fails

--- src/Building.java
+++ src/Building.java
@@ -1,13 +1,22 @@
 import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class Building {
     private int mId;
-    private String[] mResidents;
+    private List<String> mResidents = new LinkedList<String>();
     public Building(int id, String[] resident){
-        mId = id;
-        mResidents = resident;
+
+    }
+    public Building(int id, String resident){
+
     }
     public void addResident(String resident){
 
+    }
+    public int numberOfResidents(){
+        return 0;
     }
     public void removeResident(String resident){
 
--- test/BuildingUT.java
+++ test/BuildingUT.java
@@ -1,2 +1,10 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class BuildingUT {
+    @Test
+    public void numberofResidents_should_be1_adding_resident(){
+        Building building = new Building(0,"Manfred");
+        Assert.assertEquals(building.numberOfResidents(), 1);
+    }
 }
