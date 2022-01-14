be7d8b725536d5d00297c6f718bd53a665dbd480
Fritz <fritz@strauch.at>
2022-01-14 11:45:49 +0100

    Leerimplementiert

---
+++ src/Building.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+public class Building {
+    private int mId;
+    private String[] mResidents;
+    public Building(int id, String[] resident){
+        mId = id;
+        mResidents = resident;
+    }
+    public void addResident(String resident){
+
+    }
+    public void removeResident(String resident){
+
+    }
+}
---
+++ test/BuildingUT.java
@@ -0,0 +1,2 @@
+public class BuildingUT {
+}
