2a328f8fb2cd6d4cb6aef870c2765e7fd2819592
Fritz <fritz@strauch.at>
2022-01-14 11:56:31 +0100

    Test numberofResidents_should_be2_adding_two_resident ist true

--- src/Building.java
+++ src/Building.java
@@ -7,16 +7,17 @@ public class Building {
     private int mId;
     private List<String> mResidents = new LinkedList<String>();
     public Building(int id, String[] resident){
-
+        mId = id;
     }
     public Building(int id, String resident){
-
+        mId = id;
+        mResidents.add(resident);
     }
     public void addResident(String resident){
 
     }
     public int numberOfResidents(){
-        return 1;
+        return mResidents.size();
     }
     public void removeResident(String resident){
 
